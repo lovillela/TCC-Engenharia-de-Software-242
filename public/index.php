bcbe87e97a0465864b5bdd2517468bf56c2e3973
lovillela <luis.villela3@gmail.com>
2026-01-06 18:07:26 -0300

    Injeção de dependêcia no roteador, para receber o container de dependências

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/../src/Kernel/kernel.php';
 $urlPath = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 if (str_starts_with($urlPath, '/admin/')) {
-  AdminRouteHandler($urlPath);
+  AdminRouteHandler($urlPath, $dependencyContainer) ;
 }else{
-  RouteHandler($urlPath);
+  RouteHandler($urlPath, $dependencyContainer);
 }
