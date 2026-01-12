afec25f921b8fd5be371ba74cd5440775621b0c0
lovillela <luis.villela3@gmail.com>
2026-01-12 18:15:48 -0300

    Adição de métodos no user repository

--- src/Repositories/UserRepository.php
+++ src/Repositories/UserRepository.php
@@ -9,20 +9,25 @@ final class UserRepository
   private Connection $connection;
   private string $userCreatorQuery = 'INSERT INTO `users` (`username`, `email`, `password`, `permissions`, `isActive` ) 
                                       VALUES (?, ?, ?, ?, 1)';
-  private string $activeUserQuery = "SELECT `username`, `password` , `permissions`, `isActive`, `id` 
-                                        FROM `users` WHERE (isActive = ? AND username = ? AND permissions = ?)";
+  private string $deleteUserbyUsernameQuery = 'DELETE FROM `users` WHERE `username` = ?';
+  private string $userPasswordQuery = 'SELECT `password` FROM `users` WHERE `username` = ?';
+  private string $activeUserQuery = 'SELECT EXISTS(SELECT 1 FROM `users` WHERE (`username` = ?) AND (isActive = 1)) as isActive';
   private string $userExistsQuery = 'SELECT EXISTS(SELECT 1 FROM `users` WHERE `username` = ?) as usernameExists';
   private string $emailExistsQuery = 'SELECT EXISTS(SELECT 1 FROM `users` WHERE `email` = ?) as emailExists';
   private string $checkUserCurrentSession = 'SELECT * FROM `users` WHERE `username` = ?';
+  private string $getUserInfoQueryEmail = 'SELECT `id`, `username`, `email`, `password`, `isActive` FROM 
+                                          `users` WHERE (`email` = ? AND isActive = 1)';
+  private string $getUserInfoQueryUsername = 'SELECT `id`, `username`, `email`, `password`, `isActive` FROM 
+                                              `users` WHERE (`username` = ? AND isActive = 1)';
   private string $deleteUserQuery = 'DELETE FROM `users` WHERE `id` = ?';
-  private string $getUserID_Query = 'SELECT `id` FROM `users` WHERE `username` = ?';
+  private string $getUserIdByUsernameQuery = 'SELECT `id` FROM `users` WHERE `username` = ?';
   private string $getUserPostsQuery = 'SELECT `id_post` FROM `post_users` WHERE `id_user` = ?';
   
   public function __construct(Connection $connection){
       $this->connection = $connection;
   }
 
-  public function create(string $username, string $password, string $email, int $role /**Account role to be created*/) {
+  public function create(string $username, string $password, string $email, int $role /**Account role to be created*/): bool {
     
     $sqlStatment_UserCreation = $this->connection->prepare($this->userCreatorQuery);
     $sqlStatment_UserCreation->bindValue(1, $username);
@@ -33,67 +38,15 @@ final class UserRepository
     return (bool) $sqlStatment_UserCreation->executeStatement();
   }
 
-  public function delete(string $username) {
-    global $connection;
-    //So the IDE can display all the methods, etc
-    /** @var \Doctrine\DBAL\Connection $connection */
-    $connection = $connection;
+  public function delete(int $userId): bool {
 
-    $userData = $this->checkIfUserExists($connection, $username);
-
-    if(empty($userData)){
-      return (array('Status' => 0, 'Message' => 'User does not exist'));
-    }
-
-    if (!($this->checkUserSession($connection, $username)) && !($this->adminPrivilegeCheck())) {
-      return (array('Status' => 0, 'Message' => 'Operation not authorized!'));
-    }else{
-      //Since the user is requesting its account deletion or is an admin
-      //get user ID
-
-      $userID = $this->getUserID($connection, $username);
-
-      $sqlStatment_GetPosts = $connection->prepare($this->getUserPostsQuery);
-      $sqlStatment_GetPosts->bindValue(1, $userID);
-      $userPostIDs = $sqlStatment_GetPosts->executeQuery()->fetchAllAssociative();
-      
-      try {
-        $connection->beginTransaction();
-
-        foreach ($userPostIDs as $entityID) {
-          $sqlStatment_DeleteSlugMap = $connection->prepare($this->deleteSlugMapQuery);
-          $sqlStatment_DeleteSlugMap->bindValue(1, $entityID['id_post']);
-          $sqlStatment_DeleteSlugMap->bindValue(2, 'post');
-          $sqlStatment_DeleteSlugMap->executeQuery();
-        }
-
-        //post <- -> user id relationship
-        $sqlStatment_DeletePost_User = $connection->prepare($this->deletePost_UserQuery);
-        $sqlStatment_DeletePost_User->bindValue(1, $userID);
-        $sqlStatment_DeletePost_User->executeQuery();
-
-        foreach ($userPostIDs as $entityID) {
-          $sqlStatmentDeletePost = $connection->prepare($this->deletePostQuery);
-          $sqlStatmentDeletePost->bindValue(1, $entityID['id_post']);
-          $sqlStatmentDeletePost->executeQuery();
-        }
-
-          $sqlStatment_DeleteUser = $connection->prepare($this->deleteUserQuery);
-          $sqlStatment_DeleteUser->bindValue(1, $userID);
-          $sqlStatment_DeleteUser->executeQuery();
-
-          $connection->commit();
-
-        } catch (\Throwable $th) {
-          $connection->rollBack();
-          throw $th;
-        }
-
-      return (array('Status' => 1, 'Message' => 'User destroyed!'));
-    }
+    $deleteUserStmt = $this->connection->prepare($this->deleteUserQuery);
+    $deleteUserStmt->bindValue(1, $userId);
+    
+    return (bool) $deleteUserStmt->executeStatement();
   }
 
-  public function userExists(string $username): bool {
+  public function exists(string $username): bool {
 
     $queryUserExists = $this->connection->prepare($this->userExistsQuery);
     $queryUserExists->bindValue(1, $username);
@@ -103,9 +56,38 @@ final class UserRepository
 
   public function emailExists(string $email): bool {
 
-    $emailExistsQuery = $this->connection->prepare($this->userExistsQuery);
-    $emailExistsQuery->bindValue(1, $email);
+    $emailExistsStmt = $this->connection->prepare($this->emailExistsQuery);
+    $emailExistsStmt->bindValue(1, $email);
 
-    return (bool) $emailExistsQuery->executeQuery()->fetchOne();
-  }  
+    return (bool) $emailExistsStmt->executeQuery()->fetchOne();
+  }
+
+  public function findByUsername(string $username) : ?array {
+    $getUserInfoStmt = $this->connection->prepare($this->getUserInfoQueryUsername);
+    $getUserInfoStmt->bindValue(1, $username);
+
+    return $getUserInfoStmt->executeQuery()->fetchAssociative() ?: null;
+  }
+
+  public function findByEmail(string $email) : ?array {
+    $getUserInfoStmt = $this->connection->prepare($this->getUserInfoQueryEmail);
+    $getUserInfoStmt->bindValue(1, $email);
+
+    return $getUserInfoStmt->executeQuery()->fetchAssociative() ?: null;
+  }
+
+  public function findIdByUsername(string $username) {
+    $getUserIdStmt = $this->connection->prepare($this->getUserIdByUsernameQuery);
+    $getUserIdStmt->bindValue(1, $username);
+    return $getUserIdStmt->executeQuery()->fetchOne() ?: false;
+  }
+  
+  public function isActive(string $username) : bool {
+    
+    $activeUserStmt = $this->connection->prepare($this->activeUserQuery);
+    $activeUserStmt->bindValue(1, $username);
+
+    return (bool) $activeUserStmt->executeQuery()->fetchOne();
+  }
+
 }
