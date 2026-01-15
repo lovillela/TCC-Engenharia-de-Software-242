997e260facdeaa903b8609ae9b312e7489d60a19
lovillela <luis.villela3@gmail.com>
2026-01-14 22:43:43 -0300

    Renomeando para InputSanitizationService

---
+++ src/Services/InputSanitizationService.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Lovillela\BlogApp\Services;
+
+/**
+ * 
+ */
+final class InputSanitizationService{
+
+  public function urlRouteSanitize(string $url){
+    return preg_replace('/[^a-zA-Z0-9\-\/\?\=\&\_\.\%]/', '', $url);
+  }
+
+  public function urlInputSanitize(string $url){
+    return preg_replace('/[^a-zA-Z0-9\-]/', '', $url);
+  }
+  
+  public function postContentSanitize(string $content){
+
+    $allowedTags = ['p', 'a', 'b', 'br', 'code', 'h2', 'h3', 'h4', 'h5', 'h6', 'hr', 
+    'img', 'li', 'ol', 'q', 's', 'section', 'strong', 'sub', 'sytle', 'table', 'tbody', 
+    'td', 'th', 'thead', 'ul'];
+
+    return strip_tags($content, $allowedTags);
+  }
+
+  public function postTitleSanitize(string $title){
+    return trim(strip_tags($title));
+  }
+}
