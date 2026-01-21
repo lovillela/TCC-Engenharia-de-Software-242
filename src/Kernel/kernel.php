4de15e4008175926a6ba62de401d79a0fc143941
lovillela <luis.villela3@gmail.com>
2026-01-20 21:04:15 -0300

    Removendo chamadas ini_set e configuração de sessão do kernel

--- src/Kernel/kernel.php
+++ src/Kernel/kernel.php
@@ -10,25 +10,6 @@ use Lovillela\BlogApp\Services\SlugService;
 use Lovillela\BlogApp\Services\InputSanitizationService;
 use Lovillela\BlogApp\Services\SessionService;
 
-/*
-Prevents JavaScript from accessing the session cookie
-Cookie is only sent via HTTP(S) requests, not accessible via document.cookie
-*/
-ini_set('session.cookie_httponly', 1); 
-ini_set('session.cookie_secure', 1);      // HTTPS only (for production)
-
-/**PHP will reject uninitialized session IDs
-Forces session ID regeneration for unknown IDs */
-ini_set('session.use_strict_mode', 1);
-ini_set('session.cookie_samesite', 'Strict'); //CSRF Attack Prevention
-
-ini_set('session.gc_maxlifetime', 1440);
-
-if (session_status() === PHP_SESSION_NONE) {
-  session_start();
-}
-
-
 /** @var \Doctrine\DBAL\Connection $connection */
 $connection = require_once __DIR__ . '/../../src/Services/DatabaseConnectionService.php';
 
