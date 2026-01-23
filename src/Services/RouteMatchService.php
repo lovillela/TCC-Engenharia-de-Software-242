295dd1a1cd3bc0929834e2657f0977e44423901a
lovillela <luis.villela3@gmail.com>
2026-01-22 21:57:39 -0300

    Simples ajuste de espaçamento

--- src/Services/RouteMatchService.php
+++ src/Services/RouteMatchService.php
@@ -5,52 +5,53 @@ use Lovillela\BlogApp\Services\InputSanitizationService;
 
 class RouteMatchService
 {
-    private $router;
-    private $route;
-    private $dependencyContainer;
-    private InputSanitizationService $inputSanitization;
+  private $router;
+  private $route;
+  private $dependencyContainer;
+  private InputSanitizationService $inputSanitization;
 
-    public function __construct($router, $route, array $dependencyContainer) {
-      $this->router = $router;
-      $this->dependencyContainer = $dependencyContainer;
-      $this->inputSanitization = $dependencyContainer['InputSanitization'];
-      $this->route = $this->sanitize($route);
-    }
-    public function routeMatch() {
-
-      $routeMatch = $this->router->match($this->route);
-
-        if (!$routeMatch) {
-            header($_SERVER["SERVER_PROTOCOL"] . ' 404 Not Found');
-            echo 'Page not found';
-            return false;
-        } 
-            // Parse controller#method format
-        list($controller, $method) = explode(separator: '#', string: $routeMatch['target']);
-
-        $controllerClass = "Lovillela\\BlogApp\\Controllers\\{$controller}";
-
-        if (!(class_exists($controllerClass) && method_exists($controllerClass, $method))) {
-            header($_SERVER["SERVER_PROTOCOL"] . ' 404 Not Found');
-            echo 'Controller or method not found';
-            return false;
-        }
-
-        if ($controller === 'AuthController' && str_contains($this->route,  'login')) {
-          if (str_contains($this->route,  'admin')) {
-            $controller = new $controllerClass(1);
-            //new conditionals will be necessary for other logins
-          }elseif(str_contains($this->route,  '')){
-            $controller = new $controllerClass(3);
-          }
-        }else {
-          $controller = new $controllerClass($this->dependencyContainer);
-        }
-
-      return call_user_func_array([$controller, $method], $routeMatch['params']);
-    }
-
-  private function sanitize($route) {
-    return $this->inputSanitization->urlRouteSanitize($route);
+  public function __construct($router, $route, array $dependencyContainer) {
+    $this->router = $router;
+    $this->dependencyContainer = $dependencyContainer;
+    $this->inputSanitization = $dependencyContainer['InputSanitization'];
+    $this->route = $this->sanitize($route);
   }
+  public function routeMatch() {
+
+    $routeMatch = $this->router->match($this->route);
+
+    if (!$routeMatch) {
+      header($_SERVER["SERVER_PROTOCOL"] . ' 404 Not Found');
+      echo 'Page not found';
+      return false;
+    } 
+      
+      // Parse controller#method format
+    list($controller, $method) = explode(separator: '#', string: $routeMatch['target']);
+
+    $controllerClass = "Lovillela\\BlogApp\\Controllers\\{$controller}";
+
+    if (!(class_exists($controllerClass) && method_exists($controllerClass, $method))) {
+      header($_SERVER["SERVER_PROTOCOL"] . ' 404 Not Found');
+      echo 'Controller or method not found';
+      return false;
+    }
+
+    if ($controller === 'AuthController' && str_contains($this->route,  'login')) {
+      if (str_contains($this->route,  'admin')) {
+        $controller = new $controllerClass(1);
+        //new conditionals will be necessary for other logins
+      }elseif(str_contains($this->route,  '')){
+        $controller = new $controllerClass(3);
+      }
+    }else {
+      $controller = new $controllerClass($this->dependencyContainer);
+    }
+
+    return call_user_func_array([$controller, $method], $routeMatch['params']);
+  }
+
+private function sanitize($route) {
+  return $this->inputSanitization->urlRouteSanitize($route);
+}
 }  
