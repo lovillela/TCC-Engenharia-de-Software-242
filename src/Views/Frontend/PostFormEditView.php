fe2b28b7e20c46cfa25b5adc7dc170023115f3a1
lovillela <luis.villela3@gmail.com>
2026-02-18 17:34:36 -0300

    Criação do post form edit view.
    Têm a função de ser um formulário editável para posts (meio óbvio)

---
+++ src/Views/Frontend/PostFormEditView.php
@@ -0,0 +1,31 @@
+<div name="header">
+  <a href="/dashboard/">Dashboard</a>
+</div>
+
+<?php 
+  if (!empty($errorMessage)) {
+    echo($errorMessage);
+  }
+
+  if (!empty($generalMessage)) {
+    echo($generalMessage);
+  }
+?>
+
+<h1><?php echo($headerText) ?></h1>
+
+<form action="/post/edit/" method="post">
+
+  <input type="hidden" name="csrfToken" id="csrfToken" value="<?php echo($csrfToken); ?>">
+
+  Title: <input type="text" name="postTitle" id="postTitle" required> <br>
+
+  Slug: 
+  <input type="text" name="slugUrl" id="slugUrl" required> <br>
+  
+  Text: <br>
+  <textarea name="blogPost" id="blogPost" cols="30" rows="10"></textarea>
+  
+  <button type="submit">create</button>
+
+</form>
