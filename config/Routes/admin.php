ed81b7bd3b40b5166b3ab21c1c952c4d7982dd45
lovillela <luis.villela3@gmail.com>
2026-03-22 15:48:23 -0300

    Pequena correção na rota

--- config/Routes/admin.php
+++ config/Routes/admin.php
@@ -6,11 +6,12 @@ $router = new AltoRouter();
 
 $router->setBasePath('/admin/');
 
-$router->map('GET', '/', 'AdminController#index', 'admin');
-$router->map('POST', '/', 'AuthController#login');
+$router->map('GET', '', 'AdminController#index', 'admin');
+$router->map('POST', '', 'AuthController#login');
 $router->map('GET', 'dashboard/', 'AdminController#dashboard', 'adminDashboard');
 $router->map('GET', 'logout/', 'AuthController#logout');
 $router->map('GET', 'create/user/', 'AdminController#userCreatorForm');
 $router->map('POST', 'create/user/', 'AdminController#createUserAction');
+$router->map('GET', 'dashboard/list/posts', 'AdminController#getAllPosts');
 
 return $router;
