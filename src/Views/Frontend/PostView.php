79ba39980f024b7de8818f4bf947d1ff6abc4b54
lovillela <luis.villela3@gmail.com>
2026-03-24 14:46:58 -0300

    Ajustes para mostrar o título e conteúdo do texto

--- src/Views/Frontend/PostView.php
+++ src/Views/Frontend/PostView.php
@@ -11,13 +11,16 @@
 <div class="row justify-content-center">
   <div class="col-lg-12">
     <a href="/post/" class="btn btn-sm btn-secondary mb-4">« Voltar para Artigos</a>
-    <h1><?php echo($title);?></h1>
-    <?php 
-      if (isset($content)) {
+    <h1 class="display-3 text-center mb-5"><?php echo($title);?></h1>
+    
+    <div class="post-content" style="font-size: 1.1rem; line-height: 1.8;">
+      <?php 
+        if (isset($content)) {
           echo('<br>');
           echo($content);
           echo('<br>');
-      }
-    ?>
+        }
+      ?>
+    </div>
   </div>
 </div>
