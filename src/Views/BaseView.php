0f48ce33b9667361d33afa9495e3be451947ce5e
lovillela <luis.villela3@gmail.com>
2026-03-24 20:02:40 -0300

    Alterado base view para carregar quill js como recurso local

--- src/Views/BaseView.php
+++ src/Views/BaseView.php
@@ -3,10 +3,11 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link href="/assets/css/bootstrap.min.css" rel="stylesheet">
   <link href="/assets/css/main.css" rel="stylesheet">
   
   <title><?php echo($headTitle) ?></title>
+  <link rel="icon" href="data:,">
 </head>
 <body>
 
@@ -27,7 +28,7 @@
     <?php echo($renderedContent) ?>
   </div>
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="/assets/js/bootstrap.bundle.min.js"></script>
 
 </body>
 </html>
--- src/Views/Partial/QuillPartialView.php
+++ src/Views/Partial/QuillPartialView.php
@@ -1,6 +1,6 @@
 <?php ?>
 
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/quill@2.0.3/dist/quill.snow.css">
+<link rel="stylesheet" href="/assets/css/quill.snow.css">
 
 <div id="quill-container" style="height: 300px; background: #fff; border-radius: 4px;">
   <?php echo isset($blogPost) ? $blogPost : ''; ?>
@@ -8,26 +8,5 @@
 
 <input type="hidden" name="blogPost" id="quill-input">
 
-<script src="https://cdn.jsdelivr.net/npm/quill@2.0.3/dist/quill.js"></script>
-<script>
-  let quill = new Quill('#quill-container', {
-    theme: 'snow',
-    placeholder: 'Seu texto aqui',
-    modules: {
-        toolbar: [
-            ['bold', 'italic', 'underline'],
-            [{ 'list': 'ordered'}, { 'list': 'bullet' }],
-            ['link', 'clean', 'code-block']
-        ]
-    }
-  });
-
-  let quillForm = document.getElementById('quill-container').closest('form');
-
-  if (quillForm) {
-    quillForm.addEventListener('submit', function(){
-      let quillInput = document.getElementById('quill-input');
-      quillInput.value = quill.getSemanticHTML();
-    });
-  }
-</script>
+<script src="/assets/js/quill.min.js"> </script>
+<script src="/assets/js/quill-init.js"> </script>
