a5ff69400c76e10d2d4cba4b4d22f4e9c8e6bd58
lovillela <luis.villela3@gmail.com>
2026-03-23 22:16:26 -0300

    Removido edição de usuário

--- src/Views/Partial/UserListPartialView.php
+++ src/Views/Partial/UserListPartialView.php
@@ -6,7 +6,7 @@
   
   <?php else: ?>
     <?php foreach ($users as $user): ?>
-      <?php echo($user['username'] . ' ' . $user['email']);?> <a href="/dashboard/user/edit/<?php echo($user['id']);?>">Editar</a>
+      <?php echo($user['username'] . ' ' . $user['email']);?>
       <form action="<?php echo($deleteUrlAction) ?>delete/<?php echo($user['id']); ?>" method="POST">
         <input type="hidden" name="csrfToken" value="<?php echo($csrfToken); ?>">
         <button type="submit">
