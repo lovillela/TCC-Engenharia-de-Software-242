71b601d8eb3bbb8a905dfe7b59169476eb9b0221
lovillela <luis.villela3@gmail.com>
2026-03-24 14:38:09 -0300

    Justificando texto

--- src/Views/Frontend/PostView.php
+++ src/Views/Frontend/PostView.php
@@ -8,14 +8,16 @@
   }
 ?>
 
-<div class="col-lg-12">
-  <a href="/post/" class="btn btn-sm btn-secondary mb-4">« Voltar para Artigos</a>
-  <h1><?php echo($title);?></h1>
-  <?php 
-    if (isset($content)) {
-        echo('<br>');
-        echo($content);
-        echo('<br>');
-    }
-  ?>
+<div class="row justify-content-center">
+  <div class="col-lg-12">
+    <a href="/post/" class="btn btn-sm btn-secondary mb-4">« Voltar para Artigos</a>
+    <h1><?php echo($title);?></h1>
+    <?php 
+      if (isset($content)) {
+          echo('<br>');
+          echo($content);
+          echo('<br>');
+      }
+    ?>
+  </div>
 </div>
