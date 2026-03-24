d9dc0ecb0f378cc5fbd21078f79262ac8f34dd41
lovillela <luis.villela3@gmail.com>
2026-03-24 15:40:53 -0300

    Remoção do estilo inline

--- src/Views/Frontend/PostView.php
+++ src/Views/Frontend/PostView.php
@@ -13,7 +13,7 @@
     <a href="/post/" class="btn btn-sm btn-secondary mb-4">« Voltar para Artigos</a>
     <h1 class="display-3 text-center mb-5"><?php echo($title);?></h1>
     
-    <div class="post-content" style="font-size: 1.1rem; line-height: 1.8;">
+    <div class="post-content">
       <?php 
         if (isset($content)) {
           echo('<br>');
